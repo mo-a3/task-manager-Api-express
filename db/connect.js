954af4058a53b882ecc7415b5aad054af4aaf0ec
Moa <moa3@gmail.comd../>
2023-01-28 20:36:59 +0530

    crud

--- db/connect.js
+++ db/connect.js
@@ -3,4 +3,7 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://rasheda:2529@nodeexpressprojects.biarccz.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority";
 
-mongoose.connect(connectionString);
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
